8b2602368caaa0802ddcd4a91d36906a570a7d9a
desikas2007 <desikas209@gmail.com>
2025-11-23 22:24:14 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import "./styles/theme.css";
+
+export default function App() {
+  return (
+    <Router>
+      <header className="site-header card">
+        <div className="brand">
+         
+          <div>
+            <div style={{ fontWeight: 700 }}>DESIKA SEKAR</div>
+            
+          </div>
+        </div>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
